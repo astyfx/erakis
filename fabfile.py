dbb13ea71b2f15d951105408b754c3862e6f8b27
astyfx <astyfx@gmail.com>
2017-04-01 18:06:37 +0900

    fab setup

--- fabfile.py
+++ fabfile.py
@@ -3,5 +3,19 @@ from fabric.api import *
 import time
 import os
 
+def setup():
+    sudo("apt-get -q update")
+    sudo("apt-get -q -y install build-essential")
+    sudo("apt-get -q -y install python-pip")
+    sudo("apt-get -q -y install python-dev")
+    sudo("apt-get -q -y install libssl-dev")
+    sudo("apt-get -q -y install libxml2-dev")
+    sudo("apt-get -q -y install libxslt1-dev")
+    sudo("apt-get -q -y install libmariadbclient-dev")
+    sudo("apt-get -q -y install libjpeg-dev")  # For Thumbor
+    sudo("apt-get -q -y install libffi-dev")  # For pyOpenSSL
+    sudo("apt-get -q -y install nginx")
+    sudo("apt-get -q -y install sshpass")
+
 def restart():
     local("./bin/server.sh restart")
