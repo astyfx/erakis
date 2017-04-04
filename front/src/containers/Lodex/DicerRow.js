98b1af628171413b787c69b1e7c745b47efd48e4
astyfx <astyfx@gmail.com>
2017-04-04 22:00:33 +0900

    Dice image

--- front/src/containers/Lodex/DicerRow.js
+++ front/src/containers/Lodex/DicerRow.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled, { css } from 'styled-components'
 import Checkbox from '../../components/Checkbox'
 import {
   Row,
@@ -6,7 +7,50 @@ import {
   ColumnFlex,
 } from '../../components/Table'
 
+const DiceImage = styled.div`
+  display: inline-block;
+`
+
+const Dice4 = styled(DiceImage)`
+  width: 40px;
+  height: 36px;
+  background-size: 192px;
+  background-position: 152px 60px;
+  background-image: url(https://github.com/astyfx/erakis/blob/master/main/static/images/dice_4.png?raw=true);
+`
+const Dice6 = styled(DiceImage)`
+  width: 40px;
+  height: 36px;
+  background-size: 192px;
+  background-position: 152px 60px;
+  background-image: url(https://github.com/astyfx/erakis/blob/master/main/static/images/dice_6.png?raw=true);
+`
+const Dice10 = styled(DiceImage)`
+  width: 40px;
+  height: 36px;
+  background-size: 192px;
+  background-position: 152px 60px;
+  background-image: url(https://github.com/astyfx/erakis/blob/master/main/static/images/dice_10.png?raw=true);
+`
+
 class DicerRow extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.renderDice = this.renderDice.bind(this)
+  }
+
+  renderDice(dicer) {
+    if (dicer.diceType === '44') {
+      return (
+        <div>
+          <Dice4 />
+          <Dice4 />
+        </div>
+      )
+    }
+  }
+
   render() {
     const {
       item,
@@ -61,7 +105,7 @@ class DicerRow extends React.Component {
         <ColumnFlex
           onClick={e => { this.props.onClick(item) }}
         >
-          {item.diceType}
+          {this.renderDice(item)}
         </ColumnFlex>
         <ColumnFlex
           onClick={e => { this.props.onClick(item) }}
